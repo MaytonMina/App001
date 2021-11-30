b50bfa7d1d73aac2ba308f5dcaf28a7a493f8bd1
MaytonMina <mayton.mina2015@uteq.edu.ec>
2021-11-30 14:07:42 -0500

    v1.5 Final

--- app/src/main/java/com/example/app001/MainActivity.java
+++ app/src/main/java/com/example/app001/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,14 +22,25 @@ public class MainActivity extends AppCompatActivity {
          EditText txtNom = (EditText)findViewById(R.id.txtEditNombre);
          EditText txtTel = (EditText)findViewById(R.id.txtTelefono);
          EditText txtFec = (EditText)findViewById(R.id.txtFechaNac);
+        RadioButton rbF = (RadioButton)findViewById(R.id.rbFemenino);
+        RadioButton rbM = (RadioButton)findViewById(R.id.rbMasculino);
         //EditText txtSex = (EditText)findViewById(R.id);
 
         // Creamos la información a pasar entre actividades - Pares Key-Value
          Bundle b = new Bundle();
          b.putString("NOMBRE", txtNom.getText().toString());
          b.putString("TELEFONO", txtTel.getText().toString());
-        b.putString("FECHA", txtFec.getText().toString());
-        //b.putString("se", txtTel.getText().toString());
+         b.putString("FECHA", txtFec.getText().toString());
+        if (rbM.isChecked())
+        {
+            b.putString("SEXO", rbM.getText().toString());
+        }
+        else if (rbF.isChecked()){
+            b.putString("SEXO", rbF.getText().toString());
+        }
+        else {
+            b.putString("SEXO", "No definido");
+        }
         // Añadimos la información al intent
          intent.putExtras(b);
         // Iniciamos la nueva actividad
